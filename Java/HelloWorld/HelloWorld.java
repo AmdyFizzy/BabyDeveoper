dd58b3aa950b50530bb769a4642d560110fd3907
AmdyFizzy <amdyhafiz@gmail.com>
2018-04-26 14:17:15 +0500

    Java
    Java Hello World

---
+++ Java/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+// install jdk? and use this code in terminal to compile
+// go to c:\Program Files\Java\jdk-10\bin>
+// javac filename.java
+//eg: javac C:\Users\Pixxy\Documents\GitHub\BabyDeveoper\Java\HelloWorld\HelloWorld.java
