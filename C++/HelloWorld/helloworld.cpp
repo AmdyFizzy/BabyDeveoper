e8828bb0833363bad05f2730d4f7eb80a123daa1
AmdyFizzy <amdyhafiz@gmail.com>
2018-04-26 14:03:36 +0500

    HelloWorld
    1st code & compilation

---
+++ C++/HelloWorld/helloworld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  cout << "HELLO WORLD";
+  return 0;
+}
+
+// install g++/gcc and run the following in command line where this file is located...
+// g++ -o exeName(without .exe) inputfilename
+// eg: g++ -o hello helloworld.cpp
